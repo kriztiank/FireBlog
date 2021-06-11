c79cb8c255c8acb07359a6ee477bd456b824b5af
Kristian Kristiansen <kriztiank@gmail.com>
2021-06-11 08:31:59 +0200

    Update firebaseInit.js

--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -2,12 +2,7 @@
 // import 'firebase/firestore';
 
 // var firebaseConfig = {
-//   apiKey: 'AIzaSyBEf9DhUkDK-oFPzqsi9GU9RKiQ4WC957w',
-//   authDomain: 'fireblogs-e58d8.firebaseapp.com',
-//   projectId: 'fireblogs-e58d8',
-//   storageBucket: 'fireblogs-e58d8.appspot.com',
-//   messagingSenderId: '357143086292',
-//   appId: '1:357143086292:web:e23b8fc0908e466b6b2741',
+  // ENTER YOUR CONFIG HERE
 // };
 
 // const firebaseApp = firebase.intializeApp(firebaseConfig);
@@ -21,12 +16,7 @@ import 'firebase/firestore';
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: 'AIzaSyBEf9DhUkDK-oFPzqsi9GU9RKiQ4WC957w',
-  authDomain: 'fireblogs-e58d8.firebaseapp.com',
-  projectId: 'fireblogs-e58d8',
-  storageBucket: 'fireblogs-e58d8.appspot.com',
-  messagingSenderId: '357143086292',
-  appId: '1:357143086292:web:e23b8fc0908e466b6b2741',
+  // ENTER YOUR CONFIG HERE
 };
 
 // Initialize Firebase
